2c9bc13e19d92b9507061213a7c38de2253391ff
Archit Awasthi <143782101+archit-awasthi@users.noreply.github.com>
2025-07-08 22:42:32 +0530

    Create 2sum.cpp

---
+++ Arryas/2sum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp; // map to store value and index
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i}; // found the pair
+            }
+            mp[nums[i]] = i; // store current number and its index
+        }
+        return {}; // if no solution found (though problem says there's always one)
+    }
+};
+int main() {
+    Solution s;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = s.twoSum(nums, target);
+    cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    return 0;
+}
