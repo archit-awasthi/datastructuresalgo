ae6191d95400c273e012b5784daf81ac99f2fc19
Archit Awasthi <143782101+archit-awasthi@users.noreply.github.com>
2025-09-11 19:35:13 +0530

    Create sortbysign.cpp
    No matter what bestest approach is by taking another array. doing operations on same array might give you answer for one or two test cases but it doesnt guarentee the preservation of order. 
    
    so you can get 0(n) tc but you gotta give up on a lil extra space used.

---
+++ Arryas/sortbysign.cpp
@@ -0,0 +1,33 @@
+BRUTE
+Using pos[] and neg[] 
+store back in org array 
+for(int i=0;i<n;i++)
+{
+arr[2*i] = pos[i];
+arr[2*i+1] = neg[i];
+}
+
+BETTER APPROACH 
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n= nums.size();
+        vector<int> ans(n); int pos=0; int neg=1;
+       
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] > 0)
+            {
+                ans[pos] = nums[i];
+                pos += 2;
+            }
+
+            if(nums[i] < 0)
+            {
+                ans[neg] = nums[i];
+                neg += 2;
+            }
+        }
+        return ans;
+    }
+};
