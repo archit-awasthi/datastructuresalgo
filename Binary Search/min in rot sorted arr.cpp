7a5f040ad4d2cb1131d05aee02935fe976ff47fa
Archit Awasthi <143782101+archit-awasthi@users.noreply.github.com>
2025-09-26 22:41:13 +0530

    Create min in rot sorted arr.cpp

---
+++ Binary Search/min in rot sorted arr.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        int ans= INT_MAX;
+        int low = 0 ; int high = n-1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(nums[low] <= nums[mid])
+            {
+                ans = min(ans,nums[low]);
+                low= mid+1;
+            }
+            else
+            {
+                ans = min(ans,nums[mid]);
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
