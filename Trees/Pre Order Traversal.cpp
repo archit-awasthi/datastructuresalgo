eb9ec6fe2245026d2e4894bc8b89f796e938f273
Archit Awasthi <143782101+archit-awasthi@users.noreply.github.com>
2026-01-04 21:40:43 +0530

    Implement pre-order traversal for binary tree

---
+++ Trees/Pre Order Traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(!root) return res;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* n = st.top(); st.pop();
+            res.push_back(n->val);
+            if(n->right) st.push(n->right);
+            if(n->left) st.push(n->left);
+        }
+        return res;
+    }
+};
