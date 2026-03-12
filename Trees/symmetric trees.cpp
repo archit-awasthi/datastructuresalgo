15dbcf9104e5da36590d965a063a66d2f2c8a2fa
Archit Awasthi <143782101+archit-awasthi@users.noreply.github.com>
2026-03-12 18:21:54 +0530

    Implement symmetric tree check using recursion

---
+++ Trees/symmetric trees.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right)
+    {
+        if(!left && !right) return true;
+        if(!left || !right) return false;
+        if(left->val != right->val) return false;
+        return isMirror(left->left,right->right) && isMirror(left->right,right->left);
+    }
+    bool isSymmetric(TreeNode* root) 
+    {
+        return isMirror(root->left,root->right);
+    }
+};
